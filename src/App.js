3b619fbc7039f9b9c964670f30929dbdacb4277f
Jason <jayopoku19@gmail.com>
2024-11-02 16:37:06 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [info, setInfo] = useState({
+    name: "Jason Opoku",
+    studentID: "101347926"
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Fullstack Development - |</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <h3>{info.studentID}</h3>
+        <h4>{info.name}</h4>
+        <h5>George Brown College, Toronto</h5>
       </header>
     </div>
   );
